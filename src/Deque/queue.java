d3b617cddbb0975ceb5eab9e5018ea17d726f089
KYM-P <91421805+KYM-P@users.noreply.github.com>
2022-05-07 00:13:17 +0900

    commit

--- src/Deque/queue.java
+++ src/Deque/queue.java
@@ -29,12 +29,16 @@ public class queue { // Stack 과 반대의 느낌 가장 먼저 입력한 값을 먼저 출력/삭�
         integerQueue1.clear(); // 모든 값 제거
         System.out.println("clear 후 Queue");
         System.out.println(integerQueue1);
-        System.out.println(integerQueue1.offer(5));
+        System.out.println("offer 후 Queue");
+        integerQueue1.offer(6);
+        integerQueue1.offer(7);
+        System.out.println(integerQueue1);
 
         /* Queue
          * .poll() 가장 먼저 입력한 값 반환 후 제거
          * .peek() 가장 먼저 입력한 값 반환
          * .add(input) input 값을 Queue 에 추가
+         * .offer(input) input 값을 Queue 에 추가
          * .remove() 가장 먼저 입력한 값 제거
          * .remove(input) input 값 Queue 에서 제거
          * .clear() 모든 값 제거
