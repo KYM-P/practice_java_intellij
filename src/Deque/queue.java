c3a62b959083195f6d32af153db303022dcbec8a
KYM-P <91421805+KYM-P@users.noreply.github.com>
2022-05-06 23:40:16 +0900

    commit

---
+++ src/Deque/queue.java
@@ -0,0 +1,38 @@
+package Deque;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queue { // Stack 과 반대의 느낌 가장 먼저 입력한 값을 먼저 출력/삭제
+    public static void main (String[] args) {
+        Queue queue =  new LinkedList();
+        Queue<Integer> integerQueue1 =  new LinkedList<Integer>();
+        Queue<Integer> integerQueue2 =  new LinkedList<Integer>(Arrays.asList(1,2,3));
+
+        integerQueue1.add(1);
+        integerQueue1.add(2);
+        integerQueue1.add(3);
+        integerQueue1.add(4);
+        integerQueue1.add(5);
+        System.out.println("poll");
+        System.out.println(integerQueue1.poll());
+        System.out.println("remove 전 Queue"); // 가장 먼저 입력한 값 반환 후 제거
+        System.out.println(integerQueue1);
+        integerQueue1.remove(); // 가장 먼저 입력한 값 제거
+        System.out.println("remove 후 Queue");
+        System.out.println(integerQueue1);
+        integerQueue1.clear(); // 모든 값 제거
+        System.out.println("clear 후 Queue");
+        System.out.println(integerQueue1);
+        integerQueue1.peek();
+
+        /* Queue
+         * .poll() 가장 먼저 입력한 값 반환 후 제거
+         * .add(input) input 값을 Queue 에 추가
+         * .remove() 가장 먼저 입력한 값 제거
+         * .remove(input) input 값 Queue 에서 제거
+         * .clear() 모든 값 제거
+         */
+    }
+}
