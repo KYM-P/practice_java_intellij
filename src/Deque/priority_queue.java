8aaa9210b26749978de5855e755d6a5e5e6bbe07
KYM-P <k_ym02@naver.com>
2024-05-26 22:52:54 +0900

    20240526
    20240526

--- src/Deque/priority_queue.java
+++ src/Deque/priority_queue.java
@@ -17,6 +17,25 @@ public class priority_queue { // 요소는 Array 사용 [2진 트리 형 Heap 구조]
         System.out.println(pq_down); // 이진 트리 구조 유의
         System.out.println(pq_down.poll());
         System.out.println(pq_down);
+        /* offer(i) / poll(i) */
+    }
+
+    public static class obj implements Comparable<obj>{ // implements Comparable<객체명> 으로 상속
+        int x, y, count;
+        public obj(int y, int x, int count) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+        }
+        public int getx() {
+            return this.x;
+        }
+        @Override
+        public int compareTo(obj o) { // 부모 안 compareTo 함수 Override
+            return this.count - o.count; // 양수가 나오면 우선순위 밀림 -> 오름차순
+        }
+
+
     }
 
 }
